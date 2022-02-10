51574719a634d3b6575decd6d4df2919af2d9cc6
Danroy Ndung'u <danroy.mwangi@strathmore.edu>
2022-02-10 23:22:37 +0300

    Added all buttons.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+      extend: {
+        boxShadow: {
+          'inner-deep': 'inset 2px 2px 16px 0 rgba(0, 0, 0, 0.25),-4px -4px 12px 0 rgba(255, 255, 255, 0.3)',
+          'outer': '2px 2px 16px 0 rgba(0, 0, 0, 0.25),-4px -4px 12px 0 rgba(255, 255, 255, 0.3)'
+        },
+        :{
+          'h-11/12': 'height: 91.666667%',
+        },
+      },
+    plugins: [],
+  }
 }
